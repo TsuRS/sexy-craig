6b36a14dcb9ce790a49b5db9611058f8de9a3bfa
xmslvs <xmslvs@gmail.com>
2025-04-28 14:41:28 -0700

    added placeholder sections

--- src/CenterCol.js
+++ src/CenterCol.js
@@ -36,6 +36,11 @@ function InformationSections() {
       <Section  label="members" />
       <Section  label="new events" />
       <Section label="past performances" />
+      <Section label="placeholder" />
+      <Section label="placeholder" />
+      <Section label="placeholder" />
+      <Section label="placeholder" />
+      <Section label="placeholder" />
     </div>
   )
 }
@@ -45,7 +50,8 @@ function Section({SectionContent, label}) {
     "about": "hi! we are sexy craig, stanford's premiere six-person band. our focus is on pop/jazz, with an emphasis on covers and arrangements.", 
     "members": "our members are: medha on vocals, emily on drums, eddy on keyboard, sunny on guitar, and issa on bass.",
     "new events": "our first live performance is coming up on may 24th, at tressider (on the side of the package center), from 7pm-8pm!",
-    "past performances": "below is a list of our past performances, link1, link2. stay tuned for more, posted on our instagram and youtube!"
+    "past performances": "below is a list of our past performances, link1, link2. stay tuned for more, posted on our instagram and youtube!",
+    "placeholder": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
   }
   
   return(
